69aa21627aeccd8f1544f4a61256f53f604d4d0a
univ <f.lewis20.class18@outlook.com>
2023-10-17 15:19:29 +0000

    .

--- doGet.js
+++ doGet.js
@@ -110,9 +110,61 @@ var doGet = function (e) {
               <?!= subBlob ?>
               <?!= renBlob ?>`,
     {
-      renBlob: this[libName].contentApp(`<?!= appL ?>`, {
-        appL: this[libName][foobarr].apply(this, [args]),
-      }),
+      renBlob: this[libName].contentApp(
+        `          
+      <html id="doGet">
+        <head>
+          <base target="_self">
+          <meta charset="utf-8">
+          <meta name="doGet" content="Budget get Function">
+          <meta name=viewport content="width=device-width, initial-scale=1">
+          <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+          <style>
+
+              body {
+
+                flex-grow: 1;
+                color:blue;
+                text-decoration:bold;
+                flex-flow: row wrap;
+                grid-column: 1;
+                grid-row: 1;
+                text-align: center;
+                align-content: flex-start;
+                overflow: auto;
+              }
+          </style
+        </head>
+        <body>
+          <?!= appL ?>
+          <div class="row">
+            <div class="col s7 l7 m7 card-panel push-m2 push-s2 push-l2">
+              <div class="video-container"> 
+                  <iframe 
+                    src=""
+                    id="indexBeta"
+                    width="100%"
+                    height="100%"
+                    allow="autoplay"
+                    allow="encrypted-media"
+                    title="Dontime Life Website"
+                    frameborder="0"
+                    allowfullscreen
+                    ></iframe>
+              </div>
+            </div>
+          </div>
+          <script>
+            console.log(<?!= appL.length ?>)
+            if (<?!= appL.length === 99 ?>) {document.getElementById("indexBeta").src = <?= appL ?>}
+            else {document.getElementById("indexBeta").src = "https://www.clubhouse.com/@fabianlewis?utm_medium=ch_profile&utm_campaign=lhTUtHb2bYqPN3w8EEB7FQ-247242"}
+          </script>
+        </body>
+      </html>`,
+        {
+          appL: this[libName][foobarr].apply(this, [args]),
+        },
+      ),
 
       subBlob: this[libName].contentApp(`<?!= subL ?>`, {
         subL: template.evaluate().getContent(),
