5c24fc6807670e280eeee43152f7fb216c4145ec
univ <f.lewis20.class18@outlook.com>
2025-01-25 20:04:47 +0000

    .

--- doGet.js
+++ doGet.js
@@ -1,122 +1,176 @@
 var doGet = function (e) {
-  var foobarr = e.parameter["func"] || "renderFile";
   var libName = "foo";
-  var libFunc = foobarr;
-  var rndPage = [
-    `index proMedia epaWebsite callBack oddChances jsGame checkOnDay uiAccess popUpOpen congressLeg congressMembers jFundamentals gnuFree myGNUFreeJS`,
-  ]
-    .toString()
-    .split(" ")[
-    Math.floor(
-      Math.random() *
-        Math.floor(
-          [
-            `index proMedia epaWebsite callBack oddChances jsGame checkOnDay uiAccess popUpOpen congressLeg congressMembers jFundamentals gnuFree myGNUFreeJS`,
-          ]
-            .toString()
-            .split(" ").length,
-        ),
-    )
-  ];
-  var args = e.parameter["args"] || "jFundamentals";
-  var template = HtmlService.createTemplate(`<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <base target="_self" />
+  e
+    ? e
+    : (e = this[libName].objectOfS(
+        ["parameter"],
+        [[["func", arguments.callee.name]]],
+        Math.floor((this[libName].maxTime - (new Date() % (1000 * 60))) / 1000),
+      ));
+  console.log(
+    Math.floor((this[libName].maxTime - (new Date() % (1000 * 60))) / 1000) +
+      "\n" +
+      arguments.callee.name +
+      "\n!" +
+      e.parameter["func"] +
+      ", = " +
+      !e.parameter["func"],
+  );
+  var titleArray = [];
+  for (var key in globalThis) {
+    if (typeof globalThis[key] == "function") {
+      titleArray.push(key);
+    }
+  }
+  var objMaster = {
+    miscellaneous: {
+      section: titleArray,
+    },
+  };
+  var fx = e.parameter["func"];
+  if (fx === objMaster.miscellaneous.section[0]) {
+    return this[libName].handleRequest(e);
+  } else if (fx === objMaster.miscellaneous.section[5]) {
+    return this[libName].handleRequest(e);
+  } else {
+    var foobarr = "renderFile";
+    var htmlArray = [
+      `index proMedia epaWebsite callBack oddChances jsGame checkOnDay uiAccess popUpOpen congressLeg congressMembers jFundamentals gnuFree myGNUFreeJS Section3.Challenge1 cors edgarFriendly editor ssForms styling theRoll theWorks uiAccess`,
+    ]
+      .toString()
+      .split(" ");
+    var args =
+      htmlArray[Math.floor(Math.random() * Math.floor(htmlArray.length))];
+    var template = HtmlService.createTemplate(`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <base target="_self" />
 
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
-      />
-      <?!= style ?>
-    </head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+        />
+        <?!= style ?>
+      </head>
 
-    <body class=¨yellow¨>
+      <body class=¨yellow¨>
 
-      <h1><blockquote>Featured Artist:</blockquote></h1>
+        <h1><blockquote>Featured Artist:</blockquote></h1>
 
-      <p><blockquote>Larry Henry!</blockquote></p>
-    
-      <div class="yellow"; padding=0; align="center">      
-        <table id="imgSlider">
-          <tr>
-            <td>
-              
-        <div class="separator" style="clear: both;"><a href="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7" style="display: block; padding: 1em 0; text-align: left; "><img alt="" border="0" data-original-height="180" data-original-width="261" src="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7"/></a></div>
-
-            </td>
-
-            <td>
-              
-        <div class="separator" style="clear: both;"><a href="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7" style="display: block; padding: 1em 0; text-align: left; "><img alt="" border="0" data-original-height="180" data-original-width="261" src="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7"/></a></div>
-
-            </td>
-
-            <td>
-              
-        <div class="separator" style="clear: both;"><a href="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7" style="display: block; padding: 1em 0; text-align: left; "><img alt="" border="0" data-original-height="180" data-original-width="261" src="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7"/></a></div>
-
-            </td>
-          </tr>
-        </table>
-      </div>
+        <p><blockquote>Larry Henry!</blockquote></p>
       
-        <a href="%20%20%20%20%3Ch1%3E%3C?=%20foo%20?%3E%3C/h1%3E%3Cdiv%20class=%22separator%22%20style=%22clear:%20both;%22%3E%3Ca%20href=%22https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;amp;h=180&amp;amp;c=7&amp;amp;r=0&amp;amp;o=5&amp;amp;pid=1.7%22%20style=%22display:%20block;%20padding:%201em%200;%20text-align:%20center;%20%22%3E%3Cimg%20alt=%22%22%20border=%220%22%20data-original-height=%22180%22%20data-original-width=%22261%22%20src=%22https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;amp;h=180&amp;amp;c=7&amp;amp;r=0&amp;amp;o=5&amp;amp;pid=1.7%22/%3E%3C/a%3E%3C/div%3E" target="_blank" rel="nofollow"></a>
+        <div class="yellow"; padding=0; align="center">      
+          <table id="imgSlider">
+            <tr>
+              <td>
+                
+          <div class="separator" style="clear: both;"><a href="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7" style="display: block; padding: 1em 0; text-align: left; "><img alt="" border="0" data-original-height="180" data-original-width="261" src="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7"/></a></div>
 
-      <div id="slideContent" class="responsive-image">
-        <iframe
-          src="https://docs.google.com/presentation/d/e/2PACX-1vRdRMaOma2eOjpse2Drkww9jQkrTc9CiD1maR41MUfJBsnDkVR9j0wIQMjHq1jnCZIyZ4wyRMu2MnYV/embed?start=true&loop=true&delayms=2000"
-          frameborder="0"
-          width="100%"
-          height="700"
-          allowfullscreen="true"
-          mozallowfullscreen="true"
-          webkitallowfullscreen="true"
-        ></iframe>
-      </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    </body>
-  </html>`);
-  template.style = `<style>
-    body {
-      background-color: red;
-      border: 2px solid powderblue;
+              </td>
+
+              <td>
+                
+          <div class="separator" style="clear: both;"><a href="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7" style="display: block; padding: 1em 0; text-align: left; "><img alt="" border="0" data-original-height="180" data-original-width="261" src="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7"/></a></div>
+
+              </td>
+
+              <td>
+                
+          <div class="separator" style="clear: both;"><a href="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7" style="display: block; padding: 1em 0; text-align: left; "><img alt="" border="0" data-original-height="180" data-original-width="261" src="https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;h=180&amp;c=7&amp;r=0&amp;o=5&amp;pid=1.7"/></a></div>
+
+              </td>
+            </tr>
+          </table>
+        </div>
         
-    }
-    h1 {
-      color: grey;
-      fontfamily: Times New Roman;
-      fontsize: 300%;
-      margin: 10px;
+          <a href="%20%20%20%20%3Ch1%3E%3C?=%20foo%20?%3E%3C/h1%3E%3Cdiv%20class=%22separator%22%20style=%22clear:%20both;%22%3E%3Ca%20href=%22https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;amp;h=180&amp;amp;c=7&amp;amp;r=0&amp;amp;o=5&amp;amp;pid=1.7%22%20style=%22display:%20block;%20padding:%201em%200;%20text-align:%20center;%20%22%3E%3Cimg%20alt=%22%22%20border=%220%22%20data-original-height=%22180%22%20data-original-width=%22261%22%20src=%22https://th.bing.com/th/id/OIP.hg_4F_GcFIXyaWBmQMwK3QHaEo?w=261&amp;amp;h=180&amp;amp;c=7&amp;amp;r=0&amp;amp;o=5&amp;amp;pid=1.7%22/%3E%3C/a%3E%3C/div%3E" target="_blank" rel="nofollow"></a>
 
-    }
-    p {
-      color: purple;
-      fontfamily: lato;
-      background-color: gold;
-      fontsize: 160%;
-      margin: 10px;
+        <div id="slideContent" class="responsive-image">
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vRdRMaOma2eOjpse2Drkww9jQkrTc9CiD1maR41MUfJBsnDkVR9j0wIQMjHq1jnCZIyZ4wyRMu2MnYV/embed?start=true&loop=true&delayms=2000"
+            frameborder="0"
+            width="100%"
+            height="700"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+        </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      </body>
+    </html>`);
+    template.style = `<style>
+      body {
+        background-color: red;
+        border: 2px solid powderblue;
+          
+      }
+      h1 {
+        color: grey;
+        fontfamily: Times New Roman;
+        fontsize: 300%;
+        margin: 10px;
 
-    }
+      }
+      p {
+        color: purple;
+        fontfamily: lato;
+        background-color: gold;
+        fontsize: 160%;
+        margin: 10px;
 
-  </style>`;
-  return renderTemplate(
-    `<div id="pageObj"></div>
-              <script> 
-                document.addEventListener("DOMContentLoaded", eRun)
-              function eRun() {
-                document.getElementById("pageObj").innerHTML = <?!= JSON.stringify(e) ?>} 
-              </script>
-              <?!= subBlob ?>
-              <?!= renBlob ?>`,
-    {
-      renBlob: this[libName].contentApp(
-        `          
-      <html id="doGet">
+      }
+
+    </style>`;
+    if (this[libName].renderFile(args)) {
+      return renderTemplate(
+        `<html id="SubscribeDoGet">
+                <head>
+                  <base target="_self">
+                  <meta charset="utf-8">
+                  <meta name="Subscribe" content="ATL Budget Studio">
+                  <meta name=viewport content="width=device-width, initial-scale=1">
+                  <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+                  <style>
+
+                      body {
+
+                        flex-grow: 1;
+                        color:blue;
+                        text-decoration:bold;
+                        flex-flow: row wrap;
+                        grid-column: 1;
+                        grid-row: 1;
+                        text-align: center;
+                        align-content: flex-start;
+                        overflow: auto;
+                      }
+                  </style>
+                </head>
+                <body>
+                  <div>
+                    <?!= subBlob ?>
+                  </div>
+                  <div id="pageObj"></div>
+                  <div>
+                    <?!= renBlob ?>
+                  </div>
+                </body>
+                  <script> 
+                    document.addEventListener("DOMContentLoaded", eRun)
+                    function eRun() {
+                        document.getElementById("pageObj").innerHTML = <?!= JSON.stringify(e) ?>
+                      } 
+                  </script>
+              </html>`,
+        {
+          renBlob: this[libName].contentApp(
+            `          
+      <html id="Subscribe">
         <head>
           <base target="_self">
           <meta charset="utf-8">
-          <meta name="doGet" content="Budget get Function">
+          <meta name="Subscribe" content="ATL Budget Studio webapp">
           <meta name=viewport content="width=device-width, initial-scale=1">
           <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
           <style>
@@ -133,10 +187,12 @@ var doGet = function (e) {
                 align-content: flex-start;
                 overflow: auto;
               }
-          </style
+          </style>
         </head>
         <body>
-          <?!= appL ?>
+          <div id="zeroSize">
+            <?!= HtmlService.createTemplate(appL).evaluate().getContent() ?>
+          </div> 
           <div class="row">
             <div class="col s7 l7 m7 card-panel push-m2 push-s2 push-l2">
               <div class="video-container"> 
@@ -156,23 +212,61 @@ var doGet = function (e) {
           </div>
           <script>
             console.log(<?!= appL.length ?>)
-            if (<?!= appL.length === 99 ?>) {document.getElementById("indexBeta").src = <?= appL ?>}
+            if (<?!= appL.length === 99 ?>) {
+              document.getElementById("zeroSize").innerHTML = ""
+              document.getElementById("indexBeta").src = <?= appL ?>}
             else {document.getElementById("indexBeta").src = "https://www.clubhouse.com/@fabianlewis?utm_medium=ch_profile&utm_campaign=lhTUtHb2bYqPN3w8EEB7FQ-247242"}
           </script>
         </body>
-      </html>`,
-        {
-          appL: this[libName][foobarr].apply(this, [args]),
+      </html>
+      `,
+            {
+              appL: this[libName][foobarr].apply(this, [args]),
+            },
+          ),
+
+          subBlob: this[libName].contentApp(
+            `
+                    <html id="Subscribe">
+                      <head>
+                        <base target="_self">
+                        <meta charset="utf-8">
+                        <meta name="Subscribe" content="ATL Budget Studio webapp">
+                        <meta name=viewport content="width=device-width, initial-scale=1">
+                        <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+                        <style>
+
+                            body {
+
+                              flex-grow: 1;
+                              color:blue;
+                              text-decoration:bold;
+                              flex-flow: row wrap;
+                              grid-column: 1;
+                              grid-row: 1;
+                              text-align: center;
+                              align-content: flex-start;
+                              overflow: auto;
+                            }
+                        </style>
+                      </head>
+                      <body>
+                        <?!= subL ?>
+                      </body>
+                    </html>
+                        `,
+            {
+              subL: template.evaluate().getContent(),
+            },
+          ),
+
+          e: JSON.stringify(e),
         },
-      ),
-
-      subBlob: this[libName].contentApp(`<?!= subL ?>`, {
-        subL: template.evaluate().getContent(),
-      }),
-
-      e: JSON.stringify(e),
-    },
-  );
+      );
+    } else {
+      return this[libName].handleRequest(e);
+    }
+  }
 };
 
 var runBoilerplate = function (func, args) {
